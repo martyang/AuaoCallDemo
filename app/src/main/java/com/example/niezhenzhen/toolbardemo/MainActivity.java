ef60975ef850aedf319757da066ae0b5a12eeecb
martyang <quyangjus@163.com>
2017-08-20 09:41:42 +0800

    代码拆分模块化

--- app/src/main/java/com/example/niezhenzhen/toolbardemo/MainActivity.java
+++ app/src/main/java/com/example/niezhenzhen/toolbardemo/MainActivity.java
@@ -1,23 +1,19 @@
 package com.example.niezhenzhen.toolbardemo;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Environment;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.SystemClock;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -29,22 +25,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "AutoCall";
+    private String resultFolder = "AutoCall";
     private final int REQUEST_CODE = 100;
     Button add_number;
     Button start_test;
@@ -52,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     EditText number;
     EditText test_count;
     TextView test_number;
+    NavigationView navigationView;
     TelephonyManager mTelephonyManager;
     int count;
     int success = 0;
@@ -60,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     boolean isRunning = false;
     SharedPreferences sp;
+    Telephony mTelephony;
     Handler mHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
@@ -79,10 +67,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.drawerlayout);
         initView();
         numbers =new ArrayList<>();
-        if(savedInstanceState!=null){
-            Log.i(TAG,"恢复按键状态");
-            isRunning = savedInstanceState.getBoolean("isRunning");
-        }
+        mTelephony = new Telephony(this);
         /*设置toolbar*/
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -94,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         start_test.setOnClickListener(listener);
         add_number.setOnClickListener(listener);
         stop_test.setOnClickListener(listener);
+        navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener);
         /*设置导航按钮*/
 //        ActionBar actionBar = getSupportActionBar();
 //        if(actionBar != null){
@@ -111,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         number = (EditText) findViewById(R.id.phone_number);
         test_count = (EditText) findViewById(R.id.test_count);
         test_number = (TextView) findViewById(R.id.show_number);
+        navigationView = (NavigationView) findViewById(R.id.navigation);
     }
 
     private void startTest(String number){
@@ -121,10 +108,7 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
             ActivityCompat.requestPermissions(MainActivity.this,permissions,REQUEST_CODE);
         }else{
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            Uri uri = Uri.parse("tel:"+number);
-            intent.setData(uri);
-            startActivity(intent);
+              mTelephony.call(number);
         }
     }
     View.OnClickListener listener = new View.OnClickListener() {
@@ -164,27 +148,29 @@ public class MainActivity extends AppCompatActivity {
                                    if(isRunning){
                                        String number;
                                        Log.i(TAG, "拨打电话:" + i);
-                                       saveReult("自动拨电话测试结果：\n");
+                                       Utils.saveReult(resultFolder,"自动拨电话测试结果：\n");
                                        if(numbers.size()==0){
                                            number = "10086";
+                                           Utils.saveReult(resultFolder,Utils.getCurrentTime()+":start call "+number+"\n");
                                        }else {
                                            number = numbers.get(i%numbers.size());
-                                           saveReult(getCurrentTime()+":start call "+number+"\n");
+                                           Utils.saveReult(resultFolder,Utils.getCurrentTime()+":start call "+number+"\n");
                                            Log.i(TAG,"Test number:"+number);
                                        }
                                        startTest(number);
                                        try {
                                            Log.i(TAG, "delay 10 seconds");
                                            Thread.sleep(10000);
-                                           if (endCall()) {
-                                               Log.i(TAG, getCurrentTime()+"Result Success");
+                                           if (mTelephony.endCall()) {
+                                               Log.i(TAG, Utils.getCurrentTime()+"Result Success");
                                                success +=1;
-                                               saveReult(getCurrentTime()+":end call\n");
-                                               saveReult(getCurrentTime()+":Pass\n");
+                                               Utils.saveReult(resultFolder,Utils.getCurrentTime()+":end call\n");
+                                               Utils.saveReult(resultFolder,Utils.getCurrentTime()+":Pass\n");
                                            }else{
                                                Log.i(TAG,"Result Failed");
                                                fail +=1;
-                                               saveReult(getCurrentTime()+":Failed\n");
+                                               Utils.saveReult(resultFolder,Utils.getCurrentTime()+":Failed\n");
+                                               Utils.saveReult(resultFolder,Utils.getCurrentTime()+":the call has dropped or hang up\n");
                                            }
                                            Thread.sleep(10000);
                                        } catch (InterruptedException e) {
@@ -216,62 +202,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private String getCurrentTime(){
-        String current_time = "";
-//        DateFormat format = SimpleDateFormat.getDateInstance(SimpleDateFormat.DATE_FIELD);
-        SimpleDateFormat format = new SimpleDateFormat("MM-dd hh:mm:ss");
-        current_time = format.format(new Date());
-        return current_time;
-    }
-    private void saveReult(String data){
-        File result = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/AutoCall");
-        if(!result.exists()){
-            result.mkdirs();
-        }
-        File result_txt = new File(result.getAbsolutePath(),"result.txt");
-        FileOutputStream fo = null;
-        try {
-            fo = new FileOutputStream(result_txt,true);
-            fo.write(data.getBytes());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
-        }finally {
-            if(fo!=null){
-                try {
-                    fo.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * 使用反射方式调用TelephonyManager的getITelephony.endCall方法来挂断电话
-     */
-    private boolean endCall(){
-        Class<TelephonyManager> ct = TelephonyManager.class;
-        boolean isEnd = false;
-        try {
-            Method getITelephony = ct.getDeclaredMethod("getITelephony",(Class[])null);
-            getITelephony.setAccessible(true);
-            final Object iTelephony =  getITelephony.invoke(mTelephonyManager,(Object[])null);
-            Method endCall = iTelephony.getClass().getDeclaredMethod("endCall",(Class[])null);
-            endCall.setAccessible(true);
-            if(mTelephonyManager.getCallState()==TelephonyManager.CALL_STATE_OFFHOOK){
-                isEnd = (boolean) endCall.invoke(iTelephony,(Object[])null);
-            }else{
-                saveReult(getCurrentTime()+":call has dropped\n");
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return isEnd;
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode==REQUEST_CODE&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
@@ -285,15 +215,57 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.test_report:
+                    ResultBean resultBean = Utils.getReult(resultFolder);
+                    Intent intent = new Intent(MainActivity.this,ReportActivity.class);
+                    intent.putExtra("Total",resultBean.getSumCount());
+                    intent.putExtra("Pass",resultBean.getPassCount());
+                    intent.putExtra("Fail",resultBean.getFailCount());
+                    startActivity(intent);
+                    break;
+                case R.id.send_email:
+                    //send test report by email
+                    break;
+                case R.id.clear_report:
+                    if(Utils.clearData("AutoCall")){
+                        Toast.makeText(MainActivity.this,"数据已清除",Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case R.id.note:
+                    //test note
+                    break;
+
+                case R.id.author:
+
+                    break;
+                case R.id.about:
+
+                    break;
+                case R.mipmap.ic_launcher:
+                    drawerLayout.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+    };
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.test_report:
+                ResultBean resultBean = Utils.getReult(resultFolder);
                 Intent intent = new Intent(MainActivity.this,ReportActivity.class);
-                intent.putExtra("Total",count);
-                intent.putExtra("Pass",success);
-                intent.putExtra("Fail",fail);
-                startActivity(intent);
+                intent.putExtra("Total",resultBean.getSumCount());
+                intent.putExtra("Pass",resultBean.getPassCount());
+                intent.putExtra("Fail",resultBean.getFailCount());
+                break;
+            case R.id.clear_report:
+                if(Utils.clearData("AutoCall")){
+                    Toast.makeText(MainActivity.this,"数据已清除",Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.send_email:
                 //send test report by email
